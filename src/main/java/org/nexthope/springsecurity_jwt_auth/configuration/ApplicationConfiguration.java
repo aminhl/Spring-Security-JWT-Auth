0f5f407ceccb0a0499f77948c7f0ce7ba244a5e6
aminhl <amin.hlel@esprit.tn>
2025-01-26 21:02:42 +0100

    feat: adding security configuration

--- src/main/java/org/nexthope/springsecurity_jwt_auth/configuration/ApplicationConfiguration.java
+++ src/main/java/org/nexthope/springsecurity_jwt_auth/configuration/ApplicationConfiguration.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.nexthope.springsecurity_jwt_auth.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +20,19 @@ public class ApplicationConfiguration {
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findUserByEmail(username)
                 .orElse(null);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
